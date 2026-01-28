948256c8aa674b4f29d0874d6ba3c1e0eaebca6c
DivyaPeruri <divyaperuri97@gmail.com>
2026-01-28 18:05:01 +0530

    variables calling

--- variables3.sh
+++ variables3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "please enter your pin number"
-read -s PIN_NUMBER #here PIN_NUMBER is variable
+read -s PIN_NUMBER #here PIN_NUMBER is variable #password will not display if we given "s"
+
 
 echo "PIN entered is: $PIN_NUMBER"  
