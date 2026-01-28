36f6e3f10e42bf5c72147bb22f2b9f87c14b0ea0
DivyaPeruri <divyaperuri97@gmail.com>
2026-01-28 16:36:35 +0530

    variables calling

---
+++ variables2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+#printing the conversation blw ramesh and rajesh
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I'm good . What about you?"
+echo "$PERSON1:: Thanks, I'm fine. Which topic you r learning $PERSON2"
+echo "$PERSON2:: I'm learning DevSecOps $PERSON1. "
