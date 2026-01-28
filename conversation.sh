111d9a5f07a972234526903f8dbaf57b241e02f9
DivyaPeruri <divyaperuri97@gmail.com>
2026-01-28 15:30:04 +0530

    conversations file

---
+++ conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#printing the conversation blw ramesh and rajesh
+
+echo "Ramesh:: Hi Rajesh, How are you?"
+echo "Rajesh:: Hi Ramesh, I'm good . What about you?"
+echo "Ramesh:: Thanks, I'm fine. Which topic you r learning"
+echo "Rajesh:: I'm learning DevSecOps. "
