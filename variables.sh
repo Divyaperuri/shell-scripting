979a1098a3f631ba986e0296dd5e629e7c1f6910
DivyaPeruri <divyaperuri97@gmail.com>
2026-01-28 16:02:52 +0530

    vaiables script

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Rajesh
+#printing the conversation blw ramesh and rajesh
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I'm good . What about you?"
+echo "$PERSON1:: Thanks, I'm fine. Which topic you r learning"
+echo "$PERSON2:: I'm learning DevSecOps. "
