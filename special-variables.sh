f049b666df1b52e891300a78877fb9ae6b00a690
DivyaPeruri <divyaperuri97@gmail.com>
2026-01-28 19:02:31 +0530

    specialcharacters

--- special-variables.sh
+++ special-variables.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# @ , * will take all the variables at a time 
+# Space within words consider the one word , want to separate mention in " "
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
+echo "Script name: $0" 
+#Will get the file name of that script
+echo "Current directory: $PWD"
+#will get the present working directory
+echo "Who is running this: $USER"
+#will get the user details
+echo "Home directory of User: $HOME"
