42eb1af6e801f95374f158072d845e882f2d34a5
DivyaPeruri <divyaperuri97@gmail.com>
2026-01-30 18:37:10 +0530

    loops

--- 18-loops.sh
+++ 18-loops.sh
@@ -6,7 +6,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-scripting"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-LOG_FILE="LOGS_FOLDER/SCRIPT_NAME.log" #/var/log/shell-scripting/17-loops.sh
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-scripting/17-loops.sh
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
