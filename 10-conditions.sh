a96d70eb0d7d85cb58e18034fc19e365a9c98afd
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-02 11:48:23 +0530

    colors

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-NUMBER=100
+NUMBER= 80
 
-if [ $NUMBER -lt 10 ]; then
-    echo "given number $NUMBER is less than 10"
-elif [ $NUMBER -gt 10 ]; then
-    echo "given number $NUMBER is greater than 10"
+if [ $NUMBER -lt 100 ]; then
+    echo "given number $NUMBER is less than 100"
+#elif [ $NUMBER -gt 10 ]; then
+#    echo "given number $NUMBER is greater than 10"
 else
-    echo "given number $NUMBER is equal to 10"
+    echo "given number $NUMBER is equal to 100"
 fi
