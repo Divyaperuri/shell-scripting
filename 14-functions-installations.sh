9fe0ae49d9f41eebc4e5b3aea8258248be65e473
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-02 19:53:41 +0530

    colors

---
+++ 14-functions-installations.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Installing Python and Java
+$USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script in root privilage"
+    exit 1 #if it is failure
+fi
+
+VALIDATE(){ #functions recieve inputes through args just like script args
+    if[ $1 -ne 0 ]; then
+        echo "ERROR:: Installing $2 is failure"
+        exit 1
+    else 
+        echo "Installing $2 is SUCCESS"
+    fi
+}
+
+dnf install python3 -y
+VALIDATE $? "PYTHON3"
+
+dnf install java -y
+VALIDATE $? "JAVA"
