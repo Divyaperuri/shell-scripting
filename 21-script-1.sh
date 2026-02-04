2efa5c95bd63f72d19d92a13ef73efc299fe71cf
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-04 23:07:53 +0530

    catalogue changes

--- 21-script-1.sh
+++ 21-script-1.sh
@@ -6,5 +6,7 @@ echo "PID of SCRIPT-1: $$"
 #sh 22-script-2.sh
 #If we do sh , script-1 will acccess script-2 but 
 #script-2 will not access the script-1 env , because both process are different
-#If we use source script-2 can access script -1 but need to mention the path
+#If we use source script-2 can access script -1 but need to mention the path and 
+#will have full access
+
 source ./22-script-2.sh
