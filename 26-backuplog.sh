e1841bfeffac0f44938963466d9a05f02c515c41
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-06 21:54:54 +0530

    adding

--- 26-backuplog.sh
+++ 26-backuplog.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/app-logs"
+LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-scripting/17-loops.sh
 SOURCE_DIR=$1
