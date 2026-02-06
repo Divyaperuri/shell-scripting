f236376788db21bc19c342dcaa9b2492d097a78a
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-06 21:07:38 +0530

    adding

---
+++ 26-backuplog.sh
@@ -0,0 +1,27 @@
+ #!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-scripting "
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-scripting/17-loops.sh
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+     echo "ERROR:: Please run this script with root privilage"
+     exit 1 #failure is other than 0
+fi 
+
+USAGE(){
+    echo -e "USAGE: sudo sh 26-backuplogs.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[optional], default 14 days"
+    exit 1
+}  
+if [ $# -lt 2]; then
+    USAGE
+fi   
