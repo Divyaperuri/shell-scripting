3a08a4d00b5312cc0ccf8a19958c68357396ed63
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-06 21:20:51 +0530

    adding

--- 26-backuplog.sh
+++ 26-backuplog.sh
@@ -44,5 +44,5 @@ FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
 if [ ! -z "${FILES}" ]; then 
     echo "Files found"
 else
-    echo "No files to archieve.."
+    echo -e "$G No files to archieve.. $N"
 fi
