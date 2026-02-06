d950b4647fe39c56165b0bfbdd131edab95fa650
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-06 19:10:20 +0530

    adding

---
+++ 24-delete-oldlogs.sh
@@ -0,0 +1,28 @@
+ #!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-scripting"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-scripting/17-loops.sh
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+SOURCE_DIR=/home/ec2-user/app-logs
+# if [ $USERID -ne 0 ]; then
+#     echo "ERROR:: Please run this script with root privilage"
+#     exit 1 #failure is other than 0
+# fi 
+if [ ! -d $SOURCE_DIR ]; then 
+    echo -e "ERROR:: $SOURCE_DIR does not exist"
+    exit 1
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type -f -mtime +1)
+
+while IFS -read 
---
+++ 25-whilel-loop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#while loop will read the files 
+
+count=5
+echo "Starting countdown..."
+
+while [ count -gt 0 ]
+do
+    echo "Time left: $count"
+    sleep 1 # pause for 1 second
+    count=$((count -1)) #Decrement the count
+done
+
+echo "Times up!"
