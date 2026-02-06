caa909cf9bdf414f69272906aac76b60ddf35102
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-06 16:55:42 +0530

    adding another code

--- 21-script-1.sh
+++ 21-script-1.sh
@@ -3,10 +3,12 @@
 NAME=Divya
 echo "Devops Course: $NAME"
 echo "PID of SCRIPT-1: $$"
+
+# Here $$ will give you the Process id of current script
+
 #sh 22-script-2.sh
-#If we do sh , script-1 will acccess script-2 but 
-#script-2 will not access the script-1 env , because both process are different
+#If we do sh , script-1 will acccess script-2 but script-2 will not access the script-1 env , because both process are different
 #If we use source script-2 can access script -1 but need to mention the path and 
-#will have full access
+#will have full access for env
 
 source ./22-script-2.sh
--- 22-script-2.sh
+++ 22-script-2.sh
@@ -2,3 +2,5 @@
 
 echo "Devops Course: $NAME"
 echo "PID of SCRIPT-2: $$"
+
+# Here $$ will get the process id of current script 
