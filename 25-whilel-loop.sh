5676185de6317aaa77d215331cc59eab2021a985
DivyaPeruri <divyaperuri97@gmail.com>
2026-02-06 19:15:18 +0530

    adding

--- 25-whilel-loop.sh
+++ 25-whilel-loop.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 #while loop will read the files 
+#File reading line by line : IFS(Internal field separator)
+# count=5
+# echo "Starting countdown..."
 
-count=5
-echo "Starting countdown..."
+# while [ $count -gt 0 ]
+# do
+#     echo "Time left: $count"
+#     sleep 1 # pause for 1 second
+#     count=$((count -1)) #Decrement the count
+# done
 
-while [ $count -gt 0 ]
-do
-    echo "Time left: $count"
-    sleep 1 # pause for 1 second
-    count=$((count -1)) #Decrement the count
-done
+# echo "Times up!"
 
-echo "Times up!"
+while IFS= read -r line; do
+
+    echo "Processing line: $line"
+
+done < 21-script-1.sh
+# Here < symbol indicates the output taking from the file
+# <<< symbol indicates the output taking from variable 
